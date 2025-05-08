70c200bae15fe8febfe51ddc7b7bc2e737739b30
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-05-08 12:50:55 -0300

    feat: enable logs

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
+import { randomUUID } from 'node:crypto'
+
 import fastifyCors from '@fastify/cors'
 import fastify from 'fastify'
 
-const app = fastify()
+const app = fastify({
+  genReqId: () => {
+    return randomUUID()
+  },
+  logger: true,
+})
 
 app.register(fastifyCors)
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,3 @@
 import { app } from './app'
 
-app.listen({ host: '0.0.0.0', port: 3333 }).then((url) => {
-  console.log(`server is running at: ${url}`)
-})
+app.listen({ host: '0.0.0.0', port: 3333 })
