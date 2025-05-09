264872bccbe3cda58a5a6da9500cc1db97df64e8
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-05-08 21:03:34 -0300

    feat: add seed for books

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,17 +1,37 @@
 import { fakerPT_BR as faker } from '@faker-js/faker'
 
 import { db } from './connection'
-import { authors } from './schemas'
+import { authors, books } from './schemas'
 
 async function seed() {
+  await db.delete(authors)
+  await db.delete(books)
+
   const authorsData = Array.from({ length: 100 }).map(() => {
     return {
-      name: faker.person.fullName(),
+      name: faker.book.author(),
       avatar_url: faker.image.avatar(),
     }
   })
 
-  await db.insert(authors).values(authorsData)
+  const createdAuthors = await db
+    .insert(authors)
+    .values(authorsData)
+    .returning()
+
+  for (const author of createdAuthors) {
+    const booksData = Array.from({
+      length: faker.helpers.rangeToNumber({ min: 1, max: 20 }),
+    }).map(() => {
+      return {
+        author_id: author.id,
+        name: faker.book.title(),
+        cover_url: faker.image.urlLoremFlickr({ category: 'books' }),
+      }
+    })
+
+    await db.insert(books).values(booksData)
+  }
 }
 
 seed()
